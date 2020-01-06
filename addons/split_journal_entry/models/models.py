9b2286da745cb5a81eeace0c8747ae1e5e40c4e4
Mohamed Saber <mohamedabosaber94@gmail.com>
2020-01-06 03:51:00 +0200

    [FIX]split

--- addons/split_journal_entry/models/models.py
+++ addons/split_journal_entry/models/models.py
@@ -208,88 +208,88 @@ class split_journal_entry(models.Model):
             })
         return value_to_return
 
-    def _generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id):
-        # This method returns a dictonary to provide an easy extension hook to modify the valuation lines (see purchase for an example)
-        self.ensure_one()
-        rslt = {}
-        if self._context.get('forced_ref'):
-            ref = self._context['forced_ref']
-        else:
-            ref = self.picking_id.name
-        if self.serialized_candidates:
-
-            for index, item in enumerate(json.loads(self.serialized_candidates)):
-                debit_line_vals = {
-                    'name': self.name,
-                    'product_id': self.product_id.id,
-                    'quantity': item['qty'],
-                    'product_uom_id': self.product_id.uom_id.id,
-                    'ref': ref,
-                    'partner_id': partner_id,
-                    'debit': item['qty'] * item['price_unit'], #debit_value if debit_value > 0 else 0,
-                    'credit': 0,
-                    'account_id': debit_account_id,
-                }
-                credit_line_vals = {
-                'name': self.name,
-                'product_id': self.product_id.id,
-                'quantity': item['qty'],
-                'product_uom_id': self.product_id.uom_id.id,
-                'ref': ref,
-                'partner_id': partner_id,
-                'credit': item['qty'] * item['price_unit'],
-                'debit': 0,
-                'account_id': credit_account_id,
-            }
-                rslt[str(index)+'_debit'] = debit_line_vals
-                rslt[str(index) + '_credit'] = credit_line_vals
-        else:
-            debit_line_vals = {
-                'name': self.name,
-                'product_id': self.product_id.id,
-                'quantity': qty,
-                'product_uom_id': self.product_id.uom_id.id,
-                'ref': ref,
-                'partner_id': partner_id,
-                'debit': debit_value if debit_value > 0 else 0,
-                'credit': -debit_value if debit_value < 0 else 0,
-                'account_id': debit_account_id,
-            }
-            credit_line_vals = {
-            'name': self.name,
-            'product_id': self.product_id.id,
-            'quantity': qty,
-            'product_uom_id': self.product_id.uom_id.id,
-            'ref': ref,
-            'partner_id': partner_id,
-            'credit': credit_value if credit_value > 0 else 0,
-            'debit': -credit_value if credit_value < 0 else 0,
-            'account_id': credit_account_id,
-        }
-            rslt = {'credit_line_vals': credit_line_vals, 'debit_line_vals': debit_line_vals}
-
-        if credit_value != debit_value:
-            # for supplier returns of product in average costing method, in anglo saxon mode
-            diff_amount = debit_value - credit_value
-            price_diff_account = self.product_id.property_account_creditor_price_difference
-
-            if not price_diff_account:
-                price_diff_account = self.product_id.categ_id.property_account_creditor_price_difference_categ
-            if not price_diff_account:
-                raise UserError(_('Configuration error. Please configure the price difference account on the product or its category to process this operation.'))
-
-            rslt['price_diff_line_vals'] = {
-                'name': self.name,
-                'product_id': self.product_id.id,
-                'quantity': qty,
-                'product_uom_id': self.product_id.uom_id.id,
-                'ref': ref,
-                'partner_id': partner_id,
-                'credit': diff_amount > 0 and diff_amount or 0,
-                'debit': diff_amount < 0 and -diff_amount or 0,
-                'account_id': price_diff_account.id,
-            }
-        return rslt
+    # def _generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id):
+    #     # This method returns a dictonary to provide an easy extension hook to modify the valuation lines (see purchase for an example)
+    #     self.ensure_one()
+    #     rslt = {}
+    #     if self._context.get('forced_ref'):
+    #         ref = self._context['forced_ref']
+    #     else:
+    #         ref = self.picking_id.name
+    #     if self.serialized_candidates:
+    #
+    #         for index, item in enumerate(json.loads(self.serialized_candidates)):
+    #             debit_line_vals = {
+    #                 'name': self.name,
+    #                 'product_id': self.product_id.id,
+    #                 'quantity': item['qty'],
+    #                 'product_uom_id': self.product_id.uom_id.id,
+    #                 'ref': ref,
+    #                 'partner_id': partner_id,
+    #                 'debit': item['qty'] * item['price_unit'], #debit_value if debit_value > 0 else 0,
+    #                 'credit': 0,
+    #                 'account_id': debit_account_id,
+    #             }
+    #             credit_line_vals = {
+    #             'name': self.name,
+    #             'product_id': self.product_id.id,
+    #             'quantity': item['qty'],
+    #             'product_uom_id': self.product_id.uom_id.id,
+    #             'ref': ref,
+    #             'partner_id': partner_id,
+    #             'credit': item['qty'] * item['price_unit'],
+    #             'debit': 0,
+    #             'account_id': credit_account_id,
+    #         }
+    #             rslt[str(index)+'_debit'] = debit_line_vals
+    #             rslt[str(index) + '_credit'] = credit_line_vals
+    #     else:
+    #         debit_line_vals = {
+    #             'name': self.name,
+    #             'product_id': self.product_id.id,
+    #             'quantity': qty,
+    #             'product_uom_id': self.product_id.uom_id.id,
+    #             'ref': ref,
+    #             'partner_id': partner_id,
+    #             'debit': debit_value if debit_value > 0 else 0,
+    #             'credit': -debit_value if debit_value < 0 else 0,
+    #             'account_id': debit_account_id,
+    #         }
+    #         credit_line_vals = {
+    #         'name': self.name,
+    #         'product_id': self.product_id.id,
+    #         'quantity': qty,
+    #         'product_uom_id': self.product_id.uom_id.id,
+    #         'ref': ref,
+    #         'partner_id': partner_id,
+    #         'credit': credit_value if credit_value > 0 else 0,
+    #         'debit': -credit_value if credit_value < 0 else 0,
+    #         'account_id': credit_account_id,
+    #     }
+    #         rslt = {'credit_line_vals': credit_line_vals, 'debit_line_vals': debit_line_vals}
+    #
+    #     if credit_value != debit_value:
+    #         # for supplier returns of product in average costing method, in anglo saxon mode
+    #         diff_amount = debit_value - credit_value
+    #         price_diff_account = self.product_id.property_account_creditor_price_difference
+    #
+    #         if not price_diff_account:
+    #             price_diff_account = self.product_id.categ_id.property_account_creditor_price_difference_categ
+    #         if not price_diff_account:
+    #             raise UserError(_('Configuration error. Please configure the price difference account on the product or its category to process this operation.'))
+    #
+    #         rslt['price_diff_line_vals'] = {
+    #             'name': self.name,
+    #             'product_id': self.product_id.id,
+    #             'quantity': qty,
+    #             'product_uom_id': self.product_id.uom_id.id,
+    #             'ref': ref,
+    #             'partner_id': partner_id,
+    #             'credit': diff_amount > 0 and diff_amount or 0,
+    #             'debit': diff_amount < 0 and -diff_amount or 0,
+    #             'account_id': price_diff_account.id,
+    #         }
+    #     return rslt
 
     def _create_account_move_line(self, credit_account_id, debit_account_id, journal_id):
         self.ensure_one()
