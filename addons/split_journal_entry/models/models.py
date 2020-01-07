05c4acb93f41ca0d9a36f81438ccdc9480cef5aa
Mohamed Saber <mohamedabosaber94@gmail.com>
2020-01-08 01:39:17 +0200

    [FIX]split entry

--- addons/split_journal_entry/models/models.py
+++ addons/split_journal_entry/models/models.py
@@ -152,9 +152,14 @@ class StockMove(models.Model):
     def product_price_update_after_done(self):
         if self.inventory_id:
             if self.force_unit_inventory_cost or self.unit_inventory_cost:
-                self.write({
-                    'price_unit': self.force_unit_inventory_cost or self.unit_inventory_cost,
-                })
+                if self.force_unit_inventory_cost:
+                    self.write({
+                        'price_unit': self.force_unit_inventory_cost,
+                    })
+                if self.unit_inventory_cost:
+                    self.write({
+                        'price_unit': self.unit_inventory_cost,
+                    })
 
     @api.multi
     def action_done(self):
